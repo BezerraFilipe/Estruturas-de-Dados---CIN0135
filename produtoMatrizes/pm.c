b0348ea5a97b2358429049f2e288cb614c73b129
Filipe Bezerra <bezerrafilipe13@gmail.com>
2024-12-03 10:25:05 -0300

    produtoMatrizes

---
+++ produtoMatrizes/pm.c
@@ -0,0 +1,12 @@
+/*
+Atividade: Calcular o produto de duas matrizes
+
+Lê três inteiros positivos M N P no intervalo (2, 10)
+
+Cria duas matrizes aleatórias A e B de dimensões MxN e NxP com valores no intervalo (-100, +100)
+
+Calcula a matriz produto C = A*B de dimensões MxP
+
+Imprime a matrizes A, B, C
+
+*/
